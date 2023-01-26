cbf32f51f02503deef1b24ff24d86fe71f5051d3
ubisage <obaiduzair822@gmail.com>
2023-01-26 11:16:04 +0530

    used redux toolkit to simplify redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,60 @@
 import { createStore } from 'redux'
+import { createSlice } from '@reduxjs/toolkit';
 
-const counterReducer = (state = {counter : 0}, action )=>{
-    if(action.type === 'increment'){
-        return{
-            counter : state.counter + 1
-        }
+// const counterReducer = (state = {counter : 0,showCounter : true},  action )=>{
+const initialState = {counter: 0, showCounter : true}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        increment(state) {
+            state.counter++ //still have imutable code because internal emir paskage in redux toolkit
+        },
+        decrement(state) {state.counter--},
+        increase(state,action) {
+            state.counter= state.counter + action.amount
+        },
+        toggleCounter(state) {
+            state.showCounter= !state.showCounter
+        },
     }
+});
 
-    if(action.type === 'decrement'){
-        return{
-            counter : state.counter - 1
-        }
-    }
-    return state
-};
+// -------------------------------------------------------
+// used reduxtoolkit instead of this
 
-const store = createStore(counterReducer);
+// const counterReducer = (state = initialState,  action )=>{
+//     if(action.type === 'increment'){
+//         return{
+//             counter : state.counter + 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+//     if(action.type === 'increase'){
+//         return{
+//             counter : state.counter + action.amount,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+//     if(action.type === 'decrement'){
+//         return{
+//             counter : state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+//     if(action.type === 'toggle'){
+//         return{
+//             showCounter: !state.showCounter,
+//             counter : state.counter
+//         }
+//     }
+//     return state
+// };
+
+const store = createStore(counterSlice.reducer);
 
 export default store;
